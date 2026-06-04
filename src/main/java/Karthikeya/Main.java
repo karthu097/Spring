7bcd3c3a370e123b4af99af342e55d99c140b1c8
karthu097 <chandrapatikarthikeya2006gmail.com>
2026-06-04 22:19:05 +0530

    fetching the data using get method

--- src/main/java/Karthikeya/Main.java
+++ src/main/java/Karthikeya/Main.java
@@ -11,21 +11,23 @@ public class Main {
     public static void main(String[] args){
 
         Student s1 = new Student();
-        s1.setsName("gavrav");
-        s1.setRollNo(25);
-        s1.setsAge(565);
+        s1.setsName("gavr"); //dulicates are not allowed in the database
 
+        s1.setRollNo(2);
+        s1.setsAge(5555);
 
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(Karthikeya.Student.class);
-        cfg.configure( );
-        SessionFactory sf = cfg.buildSessionFactory();
+        Student s2 = null;
+
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(Karthikeya.Student.class)
+                .configure()
+                .buildSessionFactory();
         Session session = sf.openSession();
-        Transaction trans = session.beginTransaction();
 
-        session.persist(s1);
-
-        trans.commit();
+        //session.persist(s1);
+    s2 = session.get(Student.class,101);
+        session.close();
+        sf.close();
 
         System.out.println(s1);
 
