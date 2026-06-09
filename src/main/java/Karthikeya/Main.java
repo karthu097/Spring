5f8053ae401a7577c6ba6e8ca0139129c783b05c
Karthu097 <chandrapatiKarthikeya2006@gmail.com>
2026-06-10 03:12:50 +0530

    fetching the data using HQL with filter and specific features

--- src/main/java/Karthikeya/Main.java
+++ src/main/java/Karthikeya/Main.java
@@ -2,88 +2,42 @@ package Karthikeya;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import java.util.*;
+import org.hibernate.query.Query;
 
-import java.util.Arrays;
-
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
-        Laptop l1 = new Laptop();
-        l1.setLid(1);
-        l1.setBrand("lenovo");
-        l1.setModel("LOQ");
-        l1.setRam(16);
-
-        Laptop l2 = new Laptop();
-        l2.setLid(2);
-        l2.setBrand("lenovos");
-        l2.setModel("LOtQ");
-        l2.setRam(146);
-
-        Laptop l3 = new Laptop();
-        l3.setLid(3);
-        l3.setBrand("Dell");
-        l3.setModel("Alien X");
-        l3.setRam(15);
-
-
-        Alien a1 = new Alien();
-        a1.setAid(101);
-        a1.setAname("karthu");
-        a1.setTech("java");
-
-        Alien a2 = new Alien();
-        a2.setAid(102);
-        a2.setAname("Ravi");
-        a2.setTech("Python");
-
-        Alien a3 = new Alien();
-        a3.setAid(103);
-        a3.setAname("Harsh");
-        a3.setTech("C++");
-
-        a1.setLaptops(Arrays.asList(l1,l2));
-        a2.setLaptops(Arrays.asList(l2,l3));
-        a3.setLaptops(Arrays.asList(l1));
-
-
-        l1.setAliens(Arrays.asList(a1,a3));
-        l2.setAliens(Arrays.asList(a1,a2));
-        l3.setAliens(Arrays.asList(a2));
-
+    public static void main(String[] args) {
 
         SessionFactory sf = new Configuration()
-       /*  Here you need to use the class what you need for the manuplating */
-                .addAnnotatedClass(Karthikeya.Alien.class)
-                .addAnnotatedClass(Karthikeya.Laptop.class)
+                .addAnnotatedClass(Laptop.class)
+                .addAnnotatedClass(Alien.class)
                 .configure()
                 .buildSessionFactory();
+
         Session session = sf.openSession();
 
-        Transaction trans = session.beginTransaction();
+        String brand = "HP";
 
-        session.persist(l1);
-        session.persist(l2);
-        session.persist(l3);
+        Query<Object[]> query = session.createQuery(
+                "select brand, model from Laptop where brand = :brand",
+                Object[].class
+        );
 
-        session.persist(a1);
-        session.persist(a2);
-        session.persist(a3);
+        query.setParameter("brand", brand);
 
+        List<Object[]> laptops = query.getResultList();
 
-        trans.commit();
+        for (Object[] data : laptops) {
+            String laptopBrand = (String) data[0];
+            String model = (String) data[1];
 
-        Alien a5 = session.find(Alien.class,102); // get() method haves been deprecated in 7.1.0 but not removed
-        Laptop l5 = session.find(Laptop.class,101);
+            System.out.println("Brand: " + laptopBrand +
+                    ", Model: " + model);
+        }
 
-        System.out.println(a5);
         session.close();
         sf.close();
-
-
-
     }
 }
